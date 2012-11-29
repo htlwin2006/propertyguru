63af0b974b908f7a14dd04663f8d86b587ab3134
HAN TUN LWIN <htlwin2006@gmail.com>
2012-11-30 05:50:23 +0800

    fixed 4 bugs
    1. parameter parsing from REQUEST_URI obsolete since using mod_rewrite
    2. parameter (id) is fetch from GET global variable
    3. dataObj found nowhere, assuming propertyData class.
    4. json string is displayed.

--- getinfo.php
+++ getinfo.php
@@ -1,14 +1,17 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+	
 require_once("data.php");
 
-$ArrayURL = split('/', $_SERVER[REQUEST_URI]));
-$id = $ArrayURL[1];
-$data = new dataObj();
+$id = $_GET['id'];
+$data = new propertyData();
 
-if (is_object($data) = true) $status = '200 OK';
-$status_header = 'HTTP/1.1 $status';
+if (is_object($data)) $status = '200 OK';
+$status_header = "HTTP/1.1 $status";
 
 header($status_header);
-return json_encode( $data->getAll($id) );
+echo json_encode( $data->getAll($id) );
+
 
 ?>
